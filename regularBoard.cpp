b8f222ade4d411572fe34eb3e2c34718e266ecee
3890plkh <134627099+3890plkh@users.noreply.github.com>
2023-11-04 13:50:40 +0000

    Update regularBoard.cpp to help fix freechoice issue
    Changed the regular board to help fix the issue in the big board where we could choose boards where the result was already decided (added a finished attribute to the 'regular' board class).

--- regularBoard.cpp
+++ regularBoard.cpp
@@ -4,17 +4,20 @@
 
 
 // This is our board class - this is our regular tictactoe board
-// variables are the state of the board and the winner and the number of moves
+// variables are the state of the board and the winner and the number of moves and boolean boardFinished
 // class methods are makeMove - making a move, printBoard - output board to screen, and checkWinner - check to see if anyone has won
+// and isBoardFinished - check to see if a draw has occurred
 class board {
 private:
     std::vector<std::vector<std::string>> boardState = {{" "," "," "},{" "," "," "},{" "," "," "}};
 public:
     std::string winner = " "; 
+    bool boardFinished=false;
     int moves=0;
     std::vector<int> makeMove(std::string player);
     void printBoard();
     void checkWinner();
+    void isBoardFinished();
 };
 
 // this function allows the player to make a move as long as it is valid
@@ -67,15 +70,17 @@ std::vector<int> board::makeMove(std::string player) {
             }
         } 
 
-        //if the space is blank we are free to make the move for whatever player is playing
+        //if the space is blank we are free to make the move for whatever player is playing, increase number of moves by one
         // then change validMove to true to break out of loop
         // return row column for out valid move
         if (boardState[row-1][column-1]==" ") {
             if (player=="o") {
                 boardState[row-1][column-1] = "o";
+                moves++;
             } 
             else if(player=="x") {
                 boardState[row-1][column-1] = "x";
+                moves++;
             }
             validMove=true;
             rowColumn= {row,column};
@@ -91,27 +96,11 @@ std::vector<int> board::makeMove(std::string player) {
 
 // this function prints the current state of the board to the screen
 void board::printBoard() {
-    if (winner==" ") {
         std::cout << boardState[0][0] << "|" << boardState[0][1] << "|"<< boardState[0][2] << "\n";
         std::cout << "-----\n";
         std::cout << boardState[1][0] << "|" << boardState[1][1] << "|"<< boardState[1][2] << "\n";
         std::cout << "-----\n";
         std::cout << boardState[2][0] << "|" << boardState[2][1] << "|"<< boardState[2][2] << "\n";
-    }
-    else if (winner=="o") {
-        std::cout << " --- \n";
-        std::cout << "|   |\n";
-        std::cout << "|   |\n";
-        std::cout << "|   |\n";
-        std::cout << " --- \n";
-    }
-    else if (winner=="x") {
-        std::cout << "\\   /\n";
-        std::cout << " \\ / \n";
-        std::cout << "  X  \n";
-        std::cout << " / \\ \n";
-        std::cout << "/   \\" << "\n";
-    }
 }
 
 //checks if someone has won and if so changes the winner variable
@@ -139,5 +128,12 @@ void board::checkWinner() {
     }
     else if (boardState[2][0]==boardState[2][1] && boardState[2][0]==boardState[2][2] && !(boardState[2][0]==" ")) {
         winner=boardState[2][0];
+    }
+}
+
+// allows us to see if the board is finished - this function is purely for the big board
+void board::isBoardFinished() {
+    if (moves>=9 || winner!= " ") {
+        boardFinished=true;
     }
 }
